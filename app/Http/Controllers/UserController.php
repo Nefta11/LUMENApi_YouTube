2d59ac0a6d53dac8452791f328b63c75d50ff06c
Nefta11 <neftaliarturohernandez@gmail.com>
2024-05-01 13:00:07 -0600

    Updated

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::all();
-        return response()->json(["users"-> $users]);
+        return response()->json(["users"=> $users]);
     }
 
     /**
