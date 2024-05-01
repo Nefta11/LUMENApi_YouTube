3203ba2317bcf4dfd72736501738c427d0767cde
Nefta11 <neftaliarturohernandez@gmail.com>
2024-05-01 12:06:45 -0600

    Updated

--- database/migrations/2024_05_01_175131_create_users.php
+++ database/migrations/2024_05_01_175131_create_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name',50);
-            $table->string('email',50);
+            $table->string('email',50)->uniqid;
             $table->string('password',15);
             $table->string('phone',10);
             $table->boolean('status',2);
